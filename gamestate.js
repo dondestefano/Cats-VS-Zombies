b08f1a98fc317c3ada8c5078189252cd62e608cc
dondestefano <dondestefano@users.noreply.github.com>
2019-11-10 13:39:58 +0100

    Game over man
    The game over function is now working properly.

--- gamestate.js
+++ gamestate.js
@@ -1,5 +1,4 @@
 //Start the game and hide the menu
-
 function startGame()    {
     createMap();
     startingPosition();
@@ -14,4 +13,49 @@ function startGame()    {
 
 //Cat has been found and the game ends
 
+//Check if the zombie found you
+function checkGameOver(move) {
+
+    var direction = move;
+
+    if (direction = up && xPosition === xZombiePos && yPosition === yZombiePos + 1)  {
+        gameOver();
+    }
+
+    else if (direction = down && xPosition === xZombiePos && yPosition === yZombiePos - 1)  {
+        gameOver();
+    }
+
+    else if (direction = right && yPosition === yZombiePos && xPosition === xZombiePos + 1)  {
+        gameOver();
+    }
+            
+    else if (direction = left && yPosition === yZombiePos && xPosition === xZombiePos - 1)  {
+        gameOver();
+    }
+
+    else {
+        countSteps();
+        hunt();
+        flee();
+        moveLeft();
+        nextArea();
+        zombieClose();
+        catClose();
+    }
+
+}
+
 //Zombie killed you and the game ends
+function gameOver() {
+    document.getElementById("controls").style.visibility = 'hidden';
+    document.getElementById("zombie").style.visibility = 'visible';
+    document.getElementById("event").style.visibility = 'visible';
+    countSteps();
+    nextArea();
+
+}
+
+
+
+
